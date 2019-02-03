681a15f2d7529021ff49d3e25f5fd5b0ddc4bbb0
Toma Morisawa <13.35.104@gmail.com>
2019-02-03 14:26:20 +0900

    finish part 1
    https://www.gatsbyjs.org/tutorial/part-one/

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
     <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
-    <p>Such wow. Very React.</p>
+    <Link to="/">Home</Link>
+    <Header headerText="Contact" />
+    <p>Send us a message!</p>
     </div>
 )
